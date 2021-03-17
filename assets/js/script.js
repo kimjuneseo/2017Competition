715de069a631b7e0acea1b3458819f0165403b47
kimjuneseo <june3929213512@gmail.com>
2021-03-17 22:45:55 +0900

    30-17

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,48 @@
-
-const meun = document.querySelector('.nav')
+const clone = $(".contents>div:first-child").clone();
+const menu = document.querySelector('.nav')
 const content = document.querySelector('.contents')
 const jsonLocation = '../music_data.json';
 const create = (tag) => document.createElement(tag);
+let current_category = "ALL";
 $.getJSON(jsonLocation, init);
-const a = Array.from(new Set(jsonLocation))
-console.log(a)
-let music;
 
-const getMusicData = url => {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("get", jsonLocation);
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) {
-                resolve(xhr.response);
-            };
-        };
-    });
-};
-
-const createMenu = function (text) {
-    const li = create("li");
-    li.innerHTML = `
-    <a href="#"><i class="fa fa-youtube-play fa-2x"></i> <span>${text}</span></a>
-    `
-    meun.append(li)
-}
 
 function init({ data }) {
-    // const { data } = await getMusicData();
-
-    meun.childNodes[5].remove()
-    Array.from(new Set(data.map(data => data.category))).forEach(createMenu)
-    Array.from(new Set(data.map(data => data))).forEach(({ albumJaketImage, albumName, artist, release, price }) => {
-        const div = create('div');
-        div.innerHTML = `
-            <div class="col-md-2 col-sm-2 col-xs-2 product-grid">
-                            <div class="product-items">
-                                    <div class="project-eff">
-                                        <img class="img-responsive" src="images/${albumJaketImage}" alt=${albumName} >
-                                    </div>
-                                <div class="produ-cost">
-                                    <h5>${albumName}</h5>
-                                    <span>
-                                        <i class="fa fa-microphone"> </i> 
-                                        <p>${artist}</p>
-                                    </span>
-                                    <span>
-                                        <i class="fa  fa-calendar"> 발매일</i> 
-                                         
-                                        <p>${release}</p>
-                                    </span>
-                                    <span>
-                                        <i class="fa fa-money"> 가격</i>
-                                        <p>￦${price}</p>
-                                    </span>
-                                    <span class="shopbtn">
-                                        <button class="btn btn-default btn-xs">
-                                            <i class="fa fa-shopping-cart"></i> 쇼핑카트담기
-                                        </button>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>`
-        content.appendChild(div)
-    })
-};
-// init();
-console.log(meun.children[2])
-
-meun.addEventListener('click', function ({ target }) {
-    if (target.innerHTML === "발라드") {
-        console.log(target)
+    function item({ category, albumJaketImage, albumName, artist, release, price }) {
+        [...data].filter(({ release }) => console.log(release.split('.')[1].sort()))
+        const itemClone = clone.clone();
+        itemClone
+            .find(".project-eff img").attr("src", `images/${albumJaketImage}`).attr("alt", albumName).end()
+            .find("h5").text(albumName).end()
+            .find("span:nth-of-type(1) p").text(artist).end()
+            .find("span:nth-of-type(2) p").text(release).end()
+            .find("span:nth-of-type(3) p").text("\\ " + parseInt(price.replace("원", "")).toLocaleString());
+        if (current_category == category || current_category == "ALL") {
+            $(content).append(itemClone);
+        }
     }
-})
-// const highlight = () =>{
-// }
 
-console.log(meun.children);
+    const createMenu = function (text) {
+        console.log(text)
+        const li = create("li");
+        li.innerHTML = `
+    <a href="#"><i class="fa fa-youtube-play fa-2x"></i> <span>${text}</span></a>
+    `;
+        menu.append(li)
+    }
+    menu.childNodes[5].remove();
+    Array.from(new Set(data.map(data => data.category))).forEach(createMenu)
 
-// highlight()
+    const a = [...document.querySelectorAll("#main-menu li:not(:first-child)")].forEach(li => {
+        li.addEventListener("click", function (e) {
+            content.innerHTML = "";
+            current_category = this.textContent.trim();
+            data.forEach(item);
+        })
+    })
+
+    content.innerHTML = "";
+    data.forEach(item);
+};
+
+

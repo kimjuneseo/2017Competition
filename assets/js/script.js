2e06956dbd68122a98aca8c0e7d11219a8f00f1e
kimjuneseo <june3929213512@gmail.com>
2021-03-18 22:37:48 +0900

    03-18

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,57 @@
 const clone = $(".contents>div:first-child").clone();
-const menu = document.querySelector('.nav')
-const content = document.querySelector('.contents')
-const jsonLocation = '../music_data.json';
+const meun = document.querySelector('.nav');
+const meunLi = document.querySelector('.nav');
+const content = document.querySelector('.contents');
+const jsonLocation = 'music_data.json';
+const input = document.querySelectorAll('.form-control');
 const create = (tag) => document.createElement(tag);
 let current_category = "ALL";
 $.getJSON(jsonLocation, init);
-
+// const elbum = document.querySelector('') 
 
 function init({ data }) {
+    // function controllDisplay(This) {
+    //     // console.log(meun.children.classList.contains('active-menu'));
+    //     return
+    //     [...meunLi.children].forEach((element) => {
+    //         console.log(element)
+    //         element.classList.remove("active-menu");
+    //     });
+    //     This.classList.add("active-menu");
+    // }
+
+    // HTMLElement.prototype.parent = function(selector) {
+    //     let parent = this;
+    //     let check = true;
+    
+    //     const type = selector.match("#");
+    
+    //     if(type !== null) {
+    //        while(check) {
+    //           if(parent.id !== selector.replace("#", ""))
+    //              parent = parent.parentNode;
+    //           else
+    //              check = false;
+    //        };
+    
+    //        return parent;
+    //     };
+    
+    //     while(check) {
+    //        if(parent.classList.contains(selector.replace(".", ""))) {
+    //           check = false;
+    //        }else
+    //           parent = parent.parentNode;
+    //     };
+    
+    //     return parent;
+    //  };
+    
+    //  HTMLElement.prototype.find = function(target) {
+    
+    //     return this.querySelector(target);
+    // };
     function item({ category, albumJaketImage, albumName, artist, release, price }) {
-        [...data].filter(({ release }) => console.log(release.split('.')[1].sort()))
         const itemClone = clone.clone();
         itemClone
             .find(".project-eff img").attr("src", `images/${albumJaketImage}`).attr("alt", albumName).end()
@@ -21,17 +63,18 @@ function init({ data }) {
             $(content).append(itemClone);
         }
     }
-
     const createMenu = function (text) {
         console.log(text)
         const li = create("li");
         li.innerHTML = `
-    <a href="#"><i class="fa fa-youtube-play fa-2x"></i> <span>${text}</span></a>
-    `;
-        menu.append(li)
+                        <a href="#"><i class="fa fa-youtube-play fa-2x"></i> <span>${text}</span></a>
+                        `;
+        meun.append(li)
     }
-    menu.childNodes[5].remove();
+
+    meun.childNodes[5].remove();
     Array.from(new Set(data.map(data => data.category))).forEach(createMenu)
+
 
     const a = [...document.querySelectorAll("#main-menu li:not(:first-child)")].forEach(li => {
         li.addEventListener("click", function (e) {
@@ -41,8 +84,20 @@ function init({ data }) {
         })
     })
 
+
     content.innerHTML = "";
     data.forEach(item);
-};
+    console.log(input[0])
+    input[1].addEventListener("keydown", function ({ key }) {
+        console.log(this.value)
+        if (key === "Enter") {
+            content.innerHTML = "";
+            // s.innerText.includes(this.value)
+            const b = [...data].filter(({ albumName, artist, release, price }) => albumName.includes(this.value) || artist.includes(this.value) || release.includes(this.value) || price.includes(this.value)).forEach(item)
 
+            // const b = [...content.children].filter((s) => s.innerText === input.value ).forEach((e) => console.log(e))
+        }
+    })
+
+};
 
